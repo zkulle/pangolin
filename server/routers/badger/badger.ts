819a0ecb43b09c826f56f461ca0f7e22897f747a
Owen Schwartz <owen@txv.io>
2024-09-29 10:55:59 -0400

    Rename gerbil -> badger

--- server/routers/badger/badger.ts
+++ server/routers/badger/badger.ts
@@ -1,14 +1,9 @@
 import { Router } from "express";
-import { getConfig } from "./getConfig";
-import { receiveBandwidth } from "./receiveBandwidth";
 
 const badger = Router();
 
 badger.get("/", (_, res) => {
     res.status(200).json({ message: "Healthy" });
 });
-
-badger.get("/getConfig", getConfig);
-badger.post("/receiveBandwidth", receiveBandwidth);
 
 export default badger;
