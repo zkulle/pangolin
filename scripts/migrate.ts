6d843fb22cbafeba6c7283e0ac76aa7dc07e5894
Owen Schwartz <owen@txv.io>
2024-09-29 15:39:58 -0400

    Fix migrate script

--- scripts/migrate.ts
+++ scripts/migrate.ts
@@ -2,11 +2,15 @@ import { migrate } from "drizzle-orm/better-sqlite3/migrator";
 import db from "@server/db";
 import path from "path";
 
+const migrationsFolder = path.join(__dirname, "../server/migrations");
+console.log(migrationsFolder);
+
+
 const runMigrations = async () => {
     console.log("Running migrations...");
     try {
         migrate(db, {
-            migrationsFolder: path.join(__dirname, "server/migrations"),
+            migrationsFolder: migrationsFolder,
         });
         console.log("Migrations completed successfully.");
     } catch (error) {
