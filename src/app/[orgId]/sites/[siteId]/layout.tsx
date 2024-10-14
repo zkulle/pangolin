973605f54141a07a853c72d6a26c335d0713c8f2
Owen Schwartz <owen@txv.io>
2024-10-13 23:42:09 -0400

    Fix nav

--- src/app/[orgId]/sites/[siteId]/layout.tsx
+++ src/app/[orgId]/sites/[siteId]/layout.tsx
@@ -84,7 +84,7 @@ export default async function SettingsLayout({ children, params }: SettingsLayou
                 <Separator className="my-6" />
                 <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
                     <aside className="-mx-4 lg:w-1/5">
-                        <SidebarNav items={sidebarNavItems.map(i => { i.href = i.href.replace("{siteId}", params.siteId).replace("{orgId}", params.orgId); return i})} disabled={params.siteId == "create"} />
+                        <SidebarNav items={sidebarNavItems} disabled={params.siteId == "create"} />
                     </aside>
                     <div className="flex-1 lg:max-w-2xl">
                     <SiteProvider site={site}>
