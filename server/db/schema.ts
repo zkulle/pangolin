99e3ea353ab5d7bae50b9360881237bb6322cd9b
Milo Schwartz <mschwartz10612@gmail.com>
2024-10-14 21:58:34 -0400

    remove subdomain from site

--- server/db/schema.ts
+++ server/db/schema.ts
@@ -1,5 +1,5 @@
 import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
-import { InferSelectModel } from "drizzle-orm";
+import { InferSelectModel} from "drizzle-orm";
 
 export const orgs = sqliteTable("orgs", {
     orgId: text("orgId").primaryKey(),
@@ -16,7 +16,6 @@ export const sites = sqliteTable("sites", {
         onDelete: "set null",
     }),
     name: text("name").notNull(),
-    subdomain: text("subdomain"),
     pubKey: text("pubKey"),
     subnet: text("subnet"),
     megabytesIn: integer("bytesIn"),
