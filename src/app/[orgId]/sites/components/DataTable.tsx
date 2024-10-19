edde7a247a26fa1e5d433afd2f6040451ccf9645
Milo Schwartz <mschwartz10612@gmail.com>
2024-10-19 12:45:08 -0400

    adjust table styling

--- src/app/[orgId]/sites/components/DataTable.tsx
+++ src/app/[orgId]/sites/components/DataTable.tsx
@@ -79,7 +79,7 @@ export function DataTable<TData, TValue>({
                     <Plus className="mr-2 h-4 w-4" /> Add Site
                 </Button>
             </div>
-            <div className="rounded-md border">
+            <div>
                 <Table>
                     <TableHeader>
                         {table.getHeaderGroups().map((headerGroup) => (
