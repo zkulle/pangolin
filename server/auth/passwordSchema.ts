bb5573a8f485eab92967d0920eec16aa5b9afd2a
Milo Schwartz <mschwartz10612@gmail.com>
2025-01-31 15:03:36 -0500

    allow comma in password closes #121

--- server/auth/passwordSchema.ts
+++ server/auth/passwordSchema.ts
@@ -4,7 +4,7 @@ export const passwordSchema = z
     .string()
     .min(8, { message: "Password must be at least 8 characters long" })
     .max(64, { message: "Password must be at most 64 characters long" })
-    .regex(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).*$/, {
+    .regex(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[,#?!@$%^&*-]).*$/, {
         message: `Your password must meet the following conditions:
 at least one uppercase English letter,
 at least one lowercase English letter,
