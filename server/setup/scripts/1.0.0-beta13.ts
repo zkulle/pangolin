06ceff74271f519e225980fe6968ae0cdb7017b9
Milo Schwartz <mschwartz10612@gmail.com>
2025-02-12 22:29:42 -0500

    change migration script text

--- server/setup/scripts/1.0.0-beta13.ts
+++ server/setup/scripts/1.0.0-beta13.ts
@@ -1,8 +1,10 @@
 import db from "@server/db";
 import { sql } from "drizzle-orm";
 
+const version = "1.0.0-beta.13";
+
 export default async function migration() {
-    console.log("Running setup script 1.0.0-beta.13...");
+    console.log(`Running setup script ${version}...`);
 
     try {
         db.transaction((trx) => {
@@ -27,5 +29,5 @@ export default async function migration() {
         throw e;
     }
 
-    console.log("Done.");
+    console.log(`${version} migration complete`);
 }
