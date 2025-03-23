013f342ff6c2668d9daa29ead8a10ccf090e9a0d
miloschwartz <mschwartz10612@gmail.com>
2025-03-23 12:37:57 -0400

    fix broken header layout on mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -43,13 +43,7 @@ export function Header({ orgId, orgs }: HeaderProps) {
     return (
         <>
             <div className="flex items-center justify-between">
-                <div className="flex items-center gap-2">
-                    <ProfileIcon />
-
-                    <div className="hidden md:block">
-                        <SupporterStatus />
-                    </div>
-                </div>
+                <ProfileIcon />
 
                 <div className="flex items-center">
                     <div className="hidden md:block">
