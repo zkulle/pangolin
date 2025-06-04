4593edbb4595882bf3406765b9bb0df55b24d4bb
miloschwartz <mschwartz10612@gmail.com>
2025-06-04 17:28:40 -0400

    add get role to integration api

--- server/routers/role/getRole.ts
+++ server/routers/role/getRole.ts
@@ -8,12 +8,24 @@ import HttpCode from "@server/types/HttpCode";
 import createHttpError from "http-errors";
 import logger from "@server/logger";
 import { fromError } from "zod-validation-error";
+import { OpenAPITags, registry } from "@server/openApi";
 
 const getRoleSchema = z
     .object({
         roleId: z.string().transform(Number).pipe(z.number().int().positive())
     })
     .strict();
+
+registry.registerPath({
+    method: "get",
+    path: "/role/{roleId}",
+    description: "Get a role.",
+    tags: [OpenAPITags.Role],
+    request: {
+        params: getRoleSchema
+    },
+    responses: {}
+});
 
 export async function getRole(
     req: Request,
