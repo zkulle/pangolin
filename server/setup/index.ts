7b726a41ef4625a29b2bb5edf13c6ff2b925c8a9
Owen <owen@txv.io>
2025-07-16 16:56:05 -0700

    Remove extra try catch

--- server/setup/index.ts
+++ server/setup/index.ts
@@ -1,15 +1,9 @@
 import { ensureActions } from "./ensureActions";
 import { copyInConfig } from "./copyInConfig";
-import logger from "@server/logger";
 import { clearStaleData } from "./clearStaleData";
 
 export async function runSetupFunctions() {
-    try {
-        await copyInConfig(); // copy in the config to the db as needed
-        await ensureActions(); // make sure all of the actions are in the db and the roles
-        await clearStaleData();
-    } catch (error) {
-        logger.error("Error running setup functions:", error);
-        process.exit(1);
-    }
+    await copyInConfig(); // copy in the config to the db as needed
+    await ensureActions(); // make sure all of the actions are in the db and the roles
+    await clearStaleData();
 }
