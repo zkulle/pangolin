dca2a29865f263d07614ad90f6603623c110ddb3
Owen <owen@txv.io>
2025-07-18 11:32:07 -0700

    Fix #1085

--- server/routers/site/createSite.ts
+++ server/routers/site/createSite.ts
@@ -148,7 +148,7 @@ export async function createSite(
                     )
                 );
             }
-        
+
             if (!isIpInCidr(address, org.subnet)) {
                 return next(
                     createHttpError(
@@ -157,35 +157,45 @@ export async function createSite(
                     )
                 );
             }
-        
+
             updatedAddress = `${address}/${org.subnet.split("/")[1]}`; // we want the block size of the whole org
-        
+
             // make sure the subnet is unique
             const addressExistsSites = await db
                 .select()
                 .from(sites)
-                .where(eq(sites.address, updatedAddress))
+                .where(
+                    and(
+                        eq(sites.address, updatedAddress),
+                        eq(sites.orgId, orgId)
+                    )
+                )
                 .limit(1);
-        
+
             if (addressExistsSites.length > 0) {
                 return next(
                     createHttpError(
                         HttpCode.CONFLICT,
-                        `Subnet ${subnet} already exists`
+                        `Subnet ${updatedAddress} already exists in sites`
                     )
                 );
             }
-        
+
             const addressExistsClients = await db
                 .select()
                 .from(clients)
-                .where(eq(clients.subnet, updatedAddress))
+                .where(
+                    and(
+                        eq(clients.subnet, updatedAddress),
+                        eq(clients.orgId, orgId)
+                    )
+                )
                 .limit(1);
             if (addressExistsClients.length > 0) {
                 return next(
                     createHttpError(
                         HttpCode.CONFLICT,
-                        `Subnet ${subnet} already exists`
+                        `Subnet ${updatedAddress} already exists in clients`
                     )
                 );
             }
